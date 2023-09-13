773141963ead4ecf5517c12953042ad6573fd750
Kali <kali@kali>
2023-09-13 16:11:23 -0400

    creat account page is developed by using html and css (js need to be implimented)

---
+++ creat.js
@@ -0,0 +1,9 @@
+
+
+var login = document.querySelector(".login");
+var creat = document.querySelector(".creat");
+
+creat.addEventListenr("click", function(){
+    login.style.backgroundColor = "black";
+})
+
