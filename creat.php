02f2be53e71ab8ed6a734f61c23a945734a7b635
Kali <kali@kali>
2023-09-13 16:52:54 -0400

    php file is added

---
+++ creat.php
@@ -0,0 +1,14 @@
+<?php 
+    include "creat.html";
+    
+    $user = $_POST["userName"];
+    $password = $_POST["password"];
+    $length = count($users);
+    for($i =0 ; $i<length; $i++){
+        if($usets[i] == $user);
+    }
+    $users = array("Harsha"=>"Harsha1234@");
+   
+
+
+?>
